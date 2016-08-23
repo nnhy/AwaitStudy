14d71f4ab5823108d3325d57823b2e8ee688b476
大石头 <nnhy@vip.qq.com>
2016-08-23 11:47:12 +0800

    Web版本测试，出现意料之内的错误。异步模块或处理程序已完成，而当时仍有异步操作处于未定状态。

---
+++ Web/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using AwaitTest;
+
+namespace Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<ActionResult> Index()
+        {
+            Await.Start();
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
