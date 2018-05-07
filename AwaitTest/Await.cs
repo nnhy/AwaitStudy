0f0d725819e377fc5b4bed4aba35f7c59dd46ff0
大石头 <nnhy@vip.qq.com>
2018-05-07 17:48:22 +0800

    新开异步任务执行Test，等待新任务并Unwrap解包

--- AwaitTest/Await.cs
+++ AwaitTest/Await.cs
@@ -14,18 +14,37 @@ namespace AwaitTest
         public static async void Start()
         {
             WriteStep("主函数1");
-
+            // 异步执行，不等待
             Task.Run(Test);
+
             Thread.Sleep(100);
             WriteStep("主函数2");
-
+            // 同步执行第一状态机，异步执行二三状态机，并等待返回
             await Test();
+
             WriteStep("主函数3");
+            // 新开异步任务执行Test，等待新任务并Unwrap解包
+            await Task.Run(Test);
+
+            WriteStep("主函数4");
+            // 新开异步任务执行Test，等待新任务并Unwrap解包
+            Task.Run(Test).Wait();
+
+            WriteStep("主函数5");
+            // 新开异步任务执行Test，并等待新任务完成，不解包，仅执行第一状态机
+            await Task.Factory.StartNew(Test);
+
+            WriteStep("主函数6");
+            // 新开异步任务执行Test，并等待新任务完成，不解包，仅执行第一状态机
+            Task.Factory.StartNew(Test).Wait();
+
+            WriteStep("主函数7");
         }
 
-        static async Task Test()
+        static Int32 _gid;
+        static async Task<Int32> Test()
         {
-            var key = Rand.Next();
+            var key = Interlocked.Increment(ref _gid);
             WriteStep("子函数1 " + key);
 
             await Task.Delay(1300);
@@ -33,6 +52,8 @@ namespace AwaitTest
 
             await Task.Delay(1700);
             WriteStep("子函数3 " + key);
+
+            return key;
         }
 
         static void WriteStep(String name)
